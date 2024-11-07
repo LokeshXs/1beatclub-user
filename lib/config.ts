da2373b8e5dbce37f630d7269ece91d64a398f6b
Lokesh <77314004+LokeshXs@users.noreply.github.com>
2024-11-07 23:46:32 +0530

    added a new ws url

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = "http://localhost:3000"
-export const WS_SERVER_URL= "ws://1beatclub-ws.ashwacreations.com"
+export const WS_SERVER_URL= "wss://jellyfish-app-hpnvm.ondigitalocean.app"
