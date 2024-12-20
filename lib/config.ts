aa178908cc5a382fe391fc0661330ea9cf3fbdd0
Lokesh <77314004+LokeshXs@users.noreply.github.com>
2024-12-21 01:23:57 +0530

    added staging configs

--- lib/config.ts
+++ lib/config.ts
@@ -5,10 +5,13 @@ export let WS_SERVER_URL: string;
 if(process.env.ENVIRONMENT==="Devlopment"){
  BASE_URL = "http://localhost:3000"
  WS_SERVER_URL= "ws://localhost:8080"
-}else{
- BASE_URL = "https://www.1beatclub.com/"
+}else if(process.env.ENVIRONMENT==="Staging") {
+ BASE_URL = "https://1beatclub-user-git-staging-lokeshxs-projects.vercel.app/"
  WS_SERVER_URL= "wss://seal-app-yn5xl.ondigitalocean.app"
-}
+}else {
+    BASE_URL = "https://www.1beatclub.com/"
+    WS_SERVER_URL= "wss://seal-app-yn5xl.ondigitalocean.app"
+   }
 
 export  const YOUTUBE_WATCH_BASE_URL="https://www.youtube.com/watch"
 
