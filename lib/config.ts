a084e3660df49182af0dee061323577c02ccc6fc
Lokesh <lokesh1129ece@gmail.com>
2025-03-28 21:35:37 +0000

    added new websocket url

--- lib/config.ts
+++ lib/config.ts
@@ -3,10 +3,10 @@ export let WS_SERVER_URL: string;
 
 if (process.env.ENVIRONMENT === "Devlopment") {
   BASE_URL = "http://localhost:3000";
-  WS_SERVER_URL = "ws://3.108.59.30/";
+  WS_SERVER_URL = "wss://1beatclub-ws.ashwacreations.com/";
 }  else {
   BASE_URL = "https://www.1beatclub.com/";
-  WS_SERVER_URL = "ws://3.108.59.30/";
+  WS_SERVER_URL = "wss://1beatclub-ws.ashwacreations.com/";
 }
 
 export const YOUTUBE_WATCH_BASE_URL = "https://www.youtube.com/watch";
